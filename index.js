42ee5b893c1496ee23ce610022663ecd05a33f1f
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2020-05-04 00:29:42 -0400

    Umbler
    Signed-off-by: Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ app.post('/Pedido', async (req,res) => {
 
     InsertDatabase(enderecoQuery, enderecoData, inserirPedido);
 });
-  
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${config.port}!`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT , () => {
+    console.log(`Example app listening on port ${PORT}!`);
 });
